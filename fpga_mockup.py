b5282aa0578500518df82a4052c5ca11bafb43ec
mihoLinux <mdcwierz@gmail.com>
2020-07-23 16:45:37 +0200

    working

--- fpga_mockup.py
+++ fpga_mockup.py
@@ -1,21 +1,37 @@
 import socket
 
 class FpgaMockup:
-    def __init__(self, SETUP_IP = '127.0.0.1', SETUP_UDP_PORT = 12666):
-        self.SETUP_IP = SETUP_IP
-        self.SETUP_UDP_PORT = SETUP_UDP_PORT
-        self.setup_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.setup_sock.bind((SETUP_IP, SETUP_UDP_PORT))
+    def __init__(self, setup_ip = '127.0.0.1'):
+        self.setup_ip = setup_ip
+        self.setup_sock_12666 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.setup_sock_14666 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.setup_sock_15666 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.setup_sock_12666.bind((setup_ip, 12666))
+        self.setup_sock_14666.bind((setup_ip, 14666))
+        self.setup_sock_15666.bind((setup_ip, 15666))
         print('Waiting for setup...')
-        data = self.setup_sock.recvfrom(1024)[0]
-        print('Received setup, setup message: %s' % data)
+        data = self.setup_sock_12666.recvfrom(16)[0]        #TO DO: check argument
+        print(f'Received setup, setup message: {data}')
+        itnigier = int.from_bytes(data, 'big')
+        bits = "{0:b}".format(itnigier)
+        self.mode = bits[-10:-8]            #[9:8]
+        print(f'Mode set to: {self.mode}')
+        self.on = False #bool(int(bits[-8:-7]))
+        # print(self.on)
+        # if self.mode == b'01':
+        #     print("BURST!!!")
 
-    def sending(self, SPEED_TESTING_IP = '127.0.0.2', SPEED_TESTING_UDP_PORT = 5005):
+    def listening(self):
+        print("Listening...")
+
+    def sending(self, speed_testing_ip = '127.0.0.2', speed_testing_udp_port = 5005):
         print('Sending 10 packets')
         for _ in range(10):
-            self.setup_sock.sendto(b'Speed test package',
-                            (SPEED_TESTING_IP, SPEED_TESTING_UDP_PORT))
+            self.setup_sock_12666.sendto(b'Speed test package',
+                            (speed_testing_ip, speed_testing_udp_port))
 
 if __name__ == "__main__":
     testing_fpga = FpgaMockup()
+    while(testing_fpga.on == False):
+        testing_fpga.listening()
     testing_fpga.sending()
