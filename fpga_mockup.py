2b098f17a3b924decfb526bc35476b14c8ae3855
mihoLinux <mdcwierz@gmail.com>
2020-07-23 13:52:09 +0200

    initial cleaning

--- fpga_mockup.py
+++ fpga_mockup.py
@@ -1,20 +1,30 @@
 import socket
 
-SETUP_IP = '127.0.0.1'
-SETUP_UDP_PORT = 12666
-setup_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-setup_sock.bind((SETUP_IP, SETUP_UDP_PORT))
+class FpgaMockup:
+    def __init__(self, SETUP_IP, SETUP_UDP_PORT):
+        self.SETUP_IP = SETUP_IP
+        self.SETUP_UDP_PORT = SETUP_UDP_PORT
+        self.setup_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.setup_sock.bind((SETUP_IP, SETUP_UDP_PORT))
 
-SPEED_TESTING_IP = '127.0.0.2'
-SPEED_TESTING_UDP_PORT = 5005
-speed_test_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def testing():
+    SETUP_IP = '127.0.0.1'
+    SETUP_UDP_PORT = 12666
+    setup_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    setup_sock.bind((SETUP_IP, SETUP_UDP_PORT))
 
-while True:
-    data, addr = setup_sock.recvfrom(1024)
-    print('Received setup, setup message: %s' % data)
-    break
+    SPEED_TESTING_IP = '127.0.0.2'
+    SPEED_TESTING_UDP_PORT = 5005
 
-print('Sending 10 packets')
-for i in range(0, 10):
-    setup_sock.sendto(b'Speed test package',
-                      (SPEED_TESTING_IP, SPEED_TESTING_UDP_PORT))
+    while True:
+        data, addr = setup_sock.recvfrom(1024)
+        print('Received setup, setup message: %s' % data)
+        break
+
+    print('Sending 10 packets')
+    for i in range(10):
+        setup_sock.sendto(b'Speed test package',
+                        (SPEED_TESTING_IP, SPEED_TESTING_UDP_PORT))
+
+if __name__ == "__main__":
+    testing()
