cad6c2746bbb0f687db5b18e284d0cb6d5c5decf
mihoLinux <mdcwierz@gmail.com>
2020-07-24 13:36:16 +0200

    listening 3 ports with only burst mode for now

--- fpga_mockup.py
+++ fpga_mockup.py
@@ -1,4 +1,5 @@
 import socket
+import multiprocessing
 
 class FpgaMockup:
     def __init__(self, setup_ip = '127.0.0.1'):
@@ -9,30 +10,62 @@ class FpgaMockup:
         self.setup_sock_12666.bind((setup_ip, 12666))
         self.setup_sock_14666.bind((setup_ip, 14666))
         self.setup_sock_15666.bind((setup_ip, 15666))
-        print('Waiting for setup...')
-        data = self.setup_sock_12666.recvfrom(16)[0]        #TO DO: check argument
-        print(f'Received setup, setup message: {data}')
-        itnigier = int.from_bytes(data, 'big')
-        bits = "{0:b}".format(itnigier)
-        print(bits)
-        self.mode = bits[-10:-7]            #[9:8]
-        print(f'Mode set to: {self.mode}')
-        self.on = True #bool(int(bits[-8:-7]))
-        # print(self.on)
-        # if self.mode == b'01':
-        #     print("BURST!!!")
+        self.on = multiprocessing.Value('b', False)
+        self.mode = b'11'
+        self.padding = multiprocessing.Value('i', 0)
+        self.number_of_test_packets = multiprocessing.Value('i', 10)
 
-    def listening(self):
-        print("Listening...")
+    def listening_on_12666(self):
+        print("Listening on 12666...")
+        data = self.setup_sock_12666.recvfrom(16)[0]
+        print("Finished Listening on 12666.")
+
+        print(f'Received setup, setup message: {data}')
+        intiger = int.from_bytes(data, 'big')
+        bits = "{0:b}".format(intiger)
+        print(bits)
+        self.mode = bits[-10:-8]            #[9:8]
+        print(f'Mode set to: {self.mode}')
+        print(bool(int(bits[-8:-7])))
+        self.on.value = bool(int(bits[-8:-7]))
+
+    def listening_on_14666(self):
+        print("Listening on 14666...")
+        data = self.setup_sock_14666.recvfrom(16)[0]
+        print("Finished Listening on 14666.")
+
+        print(f'Received setup, setup message: {data}')
+        self.padding.value = int.from_bytes(data, 'big')
+        print(f"Set padding to {self.padding.value}")
+
+    def listening_on_15666(self):
+        print("Listening on 15666...")
+        data = self.setup_sock_15666.recvfrom(64)[0]
+        print("Finished Listening on 15666.")
+
+        print(f'Received setup, setup message: {data}')
+        self.number_of_test_packets.value = int.from_bytes(data, 'big')
+        print(f"Set number of packets to {self.number_of_test_packets.value}")
 
     def sending(self, speed_testing_ip = '127.0.0.2', speed_testing_udp_port = 5005):
-        print('Sending 10 packets')
-        for _ in range(10):
+        print(f'Sending {self.number_of_test_packets.value} packets')
+        for _ in range(self.number_of_test_packets.value):
             self.setup_sock_12666.sendto(b'Speed test package',
                             (speed_testing_ip, speed_testing_udp_port))
 
 if __name__ == "__main__":
     testing_fpga = FpgaMockup()
-    while(testing_fpga.on == False):
-        testing_fpga.listening()
+    while(testing_fpga.on.value == False):
+        p1 = multiprocessing.Process(target=testing_fpga.listening_on_12666)
+        p2 = multiprocessing.Process(target=testing_fpga.listening_on_14666)
+        p3 = multiprocessing.Process(target=testing_fpga.listening_on_15666)
+
+        p1.start()
+        p2.start()
+        p3.start()
+
+        p1.join(1)
+        p2.join(1)
+        p3.join(1)
+
     testing_fpga.sending()
