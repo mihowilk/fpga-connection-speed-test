5eda0c0cb16d5b04de33e1dc25599342c680b6b2
Konrad Walas <kwalas314@gmail.com>
2020-07-30 10:02:44 +0200

    Merge with mockup

--- package_speed_tester.py
+++ package_speed_tester.py
@@ -11,9 +11,7 @@ class FpgaConnectionSpeedTester:
 
     def __init__(self, setup_filename):
         self.setup = self.load_setup(setup_filename)
-        # self.sock_in = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock_out = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        # self.sock_in.bind((self.setup.fcst_ip, self.setup.fcst_port_in))
         self.sock_out.bind((self.setup.fcst_ip, self.setup.fcst_port_out))
 
     @staticmethod
