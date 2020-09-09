d7b62984ce2f6b11a269fa8f4e8d915b686a4bf5
Konrad Walas <kwalas314@gmail.com>
2020-09-09 15:08:17 +0200

    Fix setup packet data length

--- fcst/setup.py
+++ fcst/setup.py
@@ -31,7 +31,7 @@ class Setup:
 
     def _load_start_datagram(self, predefined_setup):
         try:
-            self.start_datagram = self._make_datagram_from_predefined_data(predefined_setup['start_datagram'])
+            self.start_datagram = self._make_datagram(predefined_setup['start_datagram'])
         except (WrongDatagramData, KeyError):
             raise StartDatagramError
 
@@ -39,31 +39,43 @@ class Setup:
         self.setup_datagrams = []
         try:
             for predefined_datagram in predefined_setup['setup_datagrams']:
-                setup_datagram = self._make_datagram_from_predefined_data(predefined_datagram)
+                setup_datagram = self._make_datagram(predefined_datagram)
                 self.setup_datagrams.append(setup_datagram)
         except (KeyError, WrongDatagramData):
             raise SetupDatagramError
 
-    def _make_datagram_from_predefined_data(self, predefined_datagram):
+    def _make_datagram(self, predefined_datagram):
         try:
             if 'data_hex' in predefined_datagram:
-                return UdpDatagram(int(predefined_datagram['data_hex'], 16).to_bytes(10, byteorder='big'),
-                                   (self.fpga_ip, predefined_datagram['fpga_port']))
-            if 'data_bin' in predefined_datagram:
-                return UdpDatagram(int(predefined_datagram['data_bin'], 2).to_bytes(10, byteorder='big'),
-                                   (self.fpga_ip, predefined_datagram['fpga_port']))
+                return self._make_datagram_from_data(predefined_datagram, int(predefined_datagram['data_hex'], 16))
             if 'data_dec' in predefined_datagram:
-                return UdpDatagram(int(predefined_datagram['data_dec'], 10).to_bytes(10, byteorder='big'),
-                                   (self.fpga_ip, predefined_datagram['fpga_port']))
+                return self._make_datagram_from_data(predefined_datagram, int(predefined_datagram['data_dec'], 10))
+            if 'data_bin' in predefined_datagram:
+                return self._make_datagram_from_data(predefined_datagram, int(predefined_datagram['data_bin'], 2))
         except KeyError:
             raise WrongDatagramData
         raise WrongDatagramData
+
+    def _make_datagram_from_data(self, predefined_datagram, data_int):
+        if 'data_length' in predefined_datagram:
+            data_length = predefined_datagram['data_length']
+        else:
+            data_length = self._calculate_data_length(data_int)
+        return UdpDatagram(data_int.to_bytes(data_length, byteorder='big'),
+                           (self.fpga_ip, predefined_datagram['fpga_port']))
 
     def is_properly_configured(self):
         if self.fpga_ip is not None and self.fcst_ip is not None and self.fcst_port_in is not None and \
                 self.setup_datagrams is not None and self.start_datagram is not None and self.fcst_port_in is not None:
             return True
         return False
+
+    @staticmethod
+    def _calculate_data_length(data_int):
+        data_length = 1
+        while 256 ** data_length < data_int:
+            data_length += 1
+        return data_length
 
 
 class UdpDatagram:
